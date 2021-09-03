e1656cbf8ae3e7e20f1c78105c1625c237bb18ee
Sunny <sunny@pragtechnologies.com>
2021-09-03 14:47:30 +0800

    Initial commit

---
+++ android/src/main/java/com/dynamsoft/flutter_ocr_sdk/FlutterOcrSdkPlugin.java
@@ -0,0 +1,107 @@
+package com.dynamsoft.flutter_ocr_sdk;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+/** FlutterOcrSdkPlugin */
+public class FlutterOcrSdkPlugin implements FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private MethodChannel channel;
+  private OCRManager mOCRManager;
+  private HandlerThread mHandlerThread;
+  private Handler mHandler;
+  private final Executor mExecutor;
+
+  public FlutterOcrSdkPlugin() {
+    mOCRManager = new OCRManager();
+    mHandler = new Handler(Looper.getMainLooper());
+    mExecutor = Executors.newSingleThreadExecutor();
+  }
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_ocr_sdk");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    switch (call.method) {
+      case "getPlatformVersion":
+        result.success("Android " + android.os.Build.VERSION.RELEASE);
+        break;
+      case "recognizeByFile": {
+        final String filename = call.argument("filename");
+        final String template = call.argument("template");
+        final Result r = result;
+        mExecutor.execute(new Runnable() {
+          @Override
+          public void run() {
+            final String results = mOCRManager.recognizeByFile(filename, template);
+            mHandler.post(new Runnable() {
+              @Override
+              public void run() {
+                r.success(results);
+              }
+            });
+
+          }
+        });
+      }
+      break;
+      case "recognizeByBuffer": {
+        final byte[] bytes = call.argument("bytes");
+        final int width = call.argument("width");
+        final int height = call.argument("height");
+        final int stride = call.argument("stride");
+        final int format = call.argument("format");
+        final String template = call.argument("template");
+        final Result r = result;
+        mExecutor.execute(new Runnable() {
+          @Override
+          public void run() {
+            final String results = mOCRManager.recognizeByBuffer(bytes, width, height, stride, format, template);
+            mHandler.post(new Runnable() {
+              @Override
+              public void run() {
+                r.success(results);
+              }
+            });
+
+          }
+        });
+      }
+      break;
+      case "loadModelFiles": {
+        final String name = call.argument("name");
+        final byte[] prototxtBuffer = call.argument("prototxtBuffer");
+        final byte[] txtBuffer = call.argument("txtBuffer");
+        final byte[] characterModelBuffer = call.argument("characterModelBuffer");
+        mOCRManager.loadModelFiles(name, prototxtBuffer, txtBuffer, characterModelBuffer);
+        result.success("");
+      }
+      break;
+      case "loadTemplate": {
+        final String template = call.argument("template");
+        mOCRManager.loadTemplate(template);
+        result.success("");
+      }
+      break;
+      default:
+        result.notImplemented();
+    }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+}
---
+++ lib/flutter_ocr_sdk.dart
@@ -0,0 +1,101 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+
+class FlutterOcrSdk {
+  static const MethodChannel _channel = const MethodChannel('flutter_ocr_sdk');
+
+  /// Returns a [String] containing the version of the platform.
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  /// Do OCR by image buffer.
+  ///
+  /// E.g. [CameraImage]
+  Future<String> recognizeByBuffer(Uint8List bytes, int width, int height,
+      int stride, int format, String template) async {
+    return await _channel.invokeMethod('recognizeByBuffer', {
+      'bytes': bytes,
+      'width': width,
+      'height': height,
+      'stride': stride,
+      'format': format,
+      'template': template,
+    });
+  }
+
+  /// Do OCR by file.
+  /// Returns a [String] containing the OCR results.
+  Future<String> recognizeByFile(String filename, String template) async {
+    return await _channel.invokeMethod('recognizeByFile', {
+      'filename': filename,
+      'template': template,
+    });
+  }
+
+  /// Load custom model files.
+  /// Returns a [String] containing the OCR results.
+  Future<void> loadModelFiles(String name, Uint8List prototxtBuffer,
+      Uint8List txtBuffer, Uint8List characterModelBuffer) async {
+    return await _channel.invokeMethod('loadModelFiles', {
+      'name': name,
+      'prototxtBuffer': prototxtBuffer,
+      'txtBuffer': txtBuffer,
+      'characterModelBuffer': characterModelBuffer,
+    });
+  }
+
+  /// Load s template file.
+  Future<void> loadTemplate(String template) async {
+    return await _channel.invokeMethod('loadTemplate', {
+      'template': template,
+    });
+  }
+
+  /// Load the whole model by folder.
+  void loadModel(String modelPath) async {
+    var fileNames = [
+      "NumberUppercase",
+      "NumberUppercase_Assist_1lIJ",
+      "NumberUppercase_Assist_8B",
+      "NumberUppercase_Assist_8BHR",
+      "NumberUppercase_Assist_number",
+      "NumberUppercase_Assist_O0DQ",
+      "NumberUppercase_Assist_upcase"
+    ];
+    for (var i = 0; i < fileNames.length; i++) {
+      var fileName = fileNames[i];
+      ByteData prototxtBuffer = await loadAssetBytes(
+          modelPath + "CharacterModel/" + fileName + ".prototxt");
+
+      ByteData txtBuffer = await loadAssetBytes(
+          modelPath + "CharacterModel/" + fileName + ".txt");
+
+      ByteData characterModelBuffer = await loadAssetBytes(
+          modelPath + "CharacterModel/" + fileName + ".caffemodel");
+
+      loadModelFiles(
+          fileName,
+          prototxtBuffer.buffer.asUint8List(),
+          txtBuffer.buffer.asUint8List(),
+          characterModelBuffer.buffer.asUint8List());
+    }
+
+    String template =
+        await loadAssetString(modelPath + 'wholeImgMRZTemplate.json');
+    loadTemplate(template);
+  }
+
+  /// Retrieve a string from the asset bundle.
+  Future<String> loadAssetString(String path) async {
+    return await rootBundle.loadString(path);
+  }
+
+  /// Retrieve a binary resource from the asset bundle as a data stream.
+  Future<ByteData> loadAssetBytes(String path) async {
+    return await rootBundle.load(path);
+  }
+}
